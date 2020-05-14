e4c936184a7982f48dcfa5cf8e3babcf01c97278
Sam Pratt <sampratt6@gmail.com>
2020-05-14 15:19:30 -0400

    Updates

---
+++ precache-manifest.0e4ede54ec2745aaf482c01f71593d93.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfbb72b099f03a901eee99e607004eb6",
+    "url": "/practice-tracker/index.html"
+  },
+  {
+    "revision": "fba93f753dd67d0e0496",
+    "url": "/practice-tracker/static/css/2.e2e81211.chunk.css"
+  },
+  {
+    "revision": "e86d429d2b6ccf613f5c",
+    "url": "/practice-tracker/static/css/main.ebd43d67.chunk.css"
+  },
+  {
+    "revision": "fba93f753dd67d0e0496",
+    "url": "/practice-tracker/static/js/2.26788250.chunk.js"
+  },
+  {
+    "revision": "683ffbbd152624aefe087d158a41ff68",
+    "url": "/practice-tracker/static/js/2.26788250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e86d429d2b6ccf613f5c",
+    "url": "/practice-tracker/static/js/main.f26b3571.chunk.js"
+  },
+  {
+    "revision": "5603cf43200d299e873a",
+    "url": "/practice-tracker/static/js/runtime-main.c4ed9770.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/practice-tracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
